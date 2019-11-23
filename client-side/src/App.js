57ea6c0af3fcbe4e705ebb9482450eb3c91060bd
Ziyad AlQahtani <ziyadmsq@gmail.com>
2019-11-23 22:41:23 +0300

    edit in the client-side

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -1,55 +1,67 @@
 import React from "react";
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
+import {Container, Card } from "react-bootstrap";
+import { Input,Select,Button } from 'antd';
 import "./App.css";
+import 'bootstrap/dist/css/bootstrap.css';
+import 'antd/dist/antd.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hill Cipher</h1>
+const InputGroup = Input.Group;
+const { Option } = Select;
 
-      <div id="inSec">
-        <p id="inputTM">Choose Key Matrix </p>
-        <br />
-        <div id="EDMenu">
-          <button id="enhButton0" type="button">
-            Encrypt
-          </button>
-          <button id="dehButton0" type="button">
-            Decrypt
-          </button>
-        </div>
+// function App() {
+class App extends React.Component {
+  state = {
+    dataSource: [],
+  };
 
-        <select id="choice">
-          <option value="m22">2 X 2 Matrix</option>
-          <option value="m33">3 X 3 Matrix</option>
-        </select>
-
-        <form id="m22">
-          <input id="tw0" type="number" name="index" min="0" max="25" />
-          <input id="tw1" type="number" name="index" min="0" max="25" /> <br />
-          <input id="tw2" type="number" name="index" min="0" max="25" />
-          <input id="tw3" type="number" name="index" min="0" max="25" />
-        </form>
-
-        <form id="m33">
-          <input id="tr0" type="number" name="index" min="0" max="25" />
-          <input id="tr1" type="number" name="index" min="0" max="25" />
-          <input id="tr2" type="number" name="index" min="0" max="25" />
-          <input id="tr3" type="number" name="index" min="0" max="25" />
-          <input id="tr4" type="number" name="index" min="0" max="25" />
-          <input id="tr5" type="number" name="index" min="0" max="25" />
-          <input id="tr6" type="number" name="index" min="0" max="25" />
-          <input id="tr7" type="number" name="index" min="0" max="25" />
-          <input id="tr8" type="number" name="index" min="0" max="25" />
-        </form>
-
-        <button id="subButtonH" type="button">
-          Submit
-        </button>
-        <br />
-      </div>
-    </div>
+  handleChange = value => {
+    this.setState({
+      dataSource:
+        !value || value.indexOf('@') >= 0
+          ? []
+          : [`${value}@gmail.com`, `${value}@163.com`, `${value}@qq.com`],
+    });
+  };
+  render(){
+    return (
+    // <>
+      // {/* <div className="App"> */}
+      <Container align="center">
+        <Card align="left" bg="info" text="white" style={{ width: 650 }}>
+          <Card.Header>Information Security</Card.Header>
+          <Card.Body>
+            <Card.Title>Defence Assignment</Card.Title>
+            <Card.Text>
+              using Node js to [] and []. 
+              for the client side i used reactjs 
+            </Card.Text>
+            <InputGroup compact>
+              <Select defaultValue="Sign Up">
+                <Option value="Sign Up">Sign Up</Option>
+                <Option value="Sign In">Sign In</Option>
+              </Select>
+              {/* <AutoComplete
+                // dataSource={this.state.dataSource}
+                style={{ width: 400 }}
+                onChange={this.handleChange}
+                placeholder="Email"
+              /> */}
+              <Input
+                style={{ width: 400 }}
+                placeholder="Email"
+                // defaultValue="input content" 
+                />
+            </InputGroup>
+            
+          </Card.Body>
+        </Card>
+      </Container>
+      // {/* </div> */}
+    // {/* </> */}
   );
+  }
+  
 }
 
 export default App;
--- test.js
+++ test.js
@@ -1,49 +1,53 @@
-[ [ 'Aaaa tctw tkDn'                            , 3       ],
-  [ 'tAkw atan btt laD eca'                     , 'table' ],
-  ['TKODE GUWMR YINPT DCFEH OQOUE ZHBXO LERJV A',"CORNELL"],
-  ['aziyad'                                     ,'abc'    ]
-].forEach( cipher => {
-    console.log(decrypt(cipher[0], cipher[1]));
+[
+  ["Aaaa tctw tkDn", 3],
+  ["tAkw atan btt laD eca", "table"],
+  ["TKODE GUWMR YINPT DCFEH OQOUE ZHBXO LERJV A", "CORNELL"],
+  ["aziyad", "abc"]
+].forEach(cipher => {
+  console.log(decrypt(cipher[0], cipher[1]));
 });
 
 function decrypt(ciphertext, key) {
-    var chars   = normalizeText(ciphertext);
-    var len     = chars.length;
-    var isStr   = typeof key === 'string';
-    var cols    = isStr ? key.length : key;
-    var rows    = Math.ceil(len / cols);
-    var mod     = divMod(len, cols)[1];
-    var matrix  = Matrix(rows, cols, '');
-    var counter = 0;
-    for (var col = 0; col < cols; col++) {
-        var remainder = mod !== 0 && col >= mod ? 1 : 0;
-        for (var row = 0; row < rows - remainder; row++) {
-            matrix[row][col] = chars[counter++];
-        }
+  var chars = normalizeText(ciphertext);
+  var len = chars.length;
+  var isStr = typeof key === "string";
+  var cols = isStr ? key.length : key;
+  var rows = Math.ceil(len / cols);
+  var mod = divMod(len, cols)[1];
+  var matrix = Matrix(rows, cols, "");
+  var counter = 0;
+  for (var col = 0; col < cols; col++) {
+    var remainder = mod !== 0 && col >= mod ? 1 : 0;
+    for (var row = 0; row < rows - remainder; row++) {
+      matrix[row][col] = chars[counter++];
     }
-    return (function(plaintext, startIndex) {
-        return plaintext.substr(startIndex);
-    }(matrix.reduce(function(result, row) {
-        return result + row.join('');
-    }, ''), isStr ? cols : 0));
+  }
+  return (function(plaintext, startIndex) {
+    return plaintext.substr(startIndex);
+  })(
+    matrix.reduce(function(result, row) {
+      return result + row.join("");
+    }, ""),
+    isStr ? cols : 0
+  );
 }
 function divMod(y, x) {
-    return [~~(y / x), y % x];
+  return [~~(y / x), y % x];
 }
 function normalizeText(ciphertext) {
-    return ciphertext.replace(/ +/g, "").split('');
+  return ciphertext.replace(/ +/g, "").split("");
 }
 function Matrix(rows, cols, defaultVal) {
-    defaultVal = defaultVal !== undefined ? defaultVal : 0;
-    var arr = [];
-    for (var i = 0; i < rows; i++) {
-        arr.push([]);
-        arr[i].push(new Array(cols));
-        for (var j = 0; j < cols; j++) {
-            arr[i][j] = defaultVal;
-        }
+  defaultVal = defaultVal !== undefined ? defaultVal : 0;
+  var arr = [];
+  for (var i = 0; i < rows; i++) {
+    arr.push([]);
+    arr[i].push(new Array(cols));
+    for (var j = 0; j < cols; j++) {
+      arr[i][j] = defaultVal;
     }
-    return arr;
+  }
+  return arr;
 }
 
-console.log(decrypt("thequickbrownfoxjumpedoverthelazydog","cornell"));
+console.log(decrypt("thequickbrownfoxjumpedoverthelazydog", "cornell"));
